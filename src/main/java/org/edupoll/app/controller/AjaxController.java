577f9e46b378f3069385aa62d8bcd45d246585bc
tomcscscs <bright.chan88@gmail.com>
2024-02-27 16:06:16 +0900

    Before 찜하기

--- src/main/java/org/edupoll/app/controller/AjaxController.java
+++ src/main/java/org/edupoll/app/controller/AjaxController.java
@@ -19,32 +19,25 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class AjaxController {
-    
-    private final AccountRepository accountRepository;
-    private final PicksRepository picksRepository;
-    
-    @PostMapping("/ajax/pickAjax")
-    @ResponseBody
-    public Map<String, String> processPickController(@RequestBody PicksCommand cmd, Authentication authentication, Model model) {
-        // Picks 객체 생성 및 저장
-        Picks one = Picks.builder()
-            .movieId(cmd.getMovieId())
-            .originalTitle(cmd.getMovieTitle())
-            .releaseDate(cmd.getTiming())
-            .posterPath(cmd.getPosterPath())
-            .accounts(accountRepository.findByUsername(authentication.getName()).orElse(null))
-            .build();
-        
-        
-        
-        picksRepository.save(one);
+
+	private final AccountRepository accountRepository;
+	private final PicksRepository picksRepository;
+
+	@PostMapping("/ajax/pickAjax")
+	@ResponseBody
+	public Map<String, String> processPickController(@RequestBody PicksCommand cmd, Authentication authentication,
+			Model model) {
+		// Picks 객체 생성 및 저장
+		Picks one = Picks.builder().movieId(cmd.getMovieId()).originalTitle(cmd.getMovieTitle())
+				.releaseDate(cmd.getTiming()).posterPath(cmd.getPosterPath())
+				.accounts(accountRepository.findByUsername(authentication.getName()).orElse(null)).build();
+
+		picksRepository.save(one);
 
 		// 클라이언트에게 성공적인 응답 반환
-        Map<String, String> response = new HashMap<>();
-        response.put("result", "success");
-        
-        
-        
-        return response;
-    }
+		Map<String, String> response = new HashMap<>();
+		response.put("result", "success");
+
+		return response;
+	}
 }
